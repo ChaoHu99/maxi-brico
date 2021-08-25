79875ce4f7ec104b3a2fb5684cfff00331ece34b
ChaoHu99 <wsl@Ideapad520.localdomain>
2021-08-25 10:07:36 +0200

    Logout completado

---
+++ frontend/src/views/Logout.vue
@@ -0,0 +1,17 @@
+  
+<template>
+  <div class="log">
+  </div>
+</template>
+
+<script>
+import store from '@/store.js'
+export default {
+    created () {
+      store.dispatch('userLogout')
+        .then(() => {
+          this.$router.push({ name: 'Login' })
+        })
+    }
+  }
+</script>
